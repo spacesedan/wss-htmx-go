4cca92ce985b98712fe4c7d60415a5cec04b8c0d
Jose Duarte <jduarte0912@gmail.com>
2023-08-24 22:23:43 -0700

    fix: there is a bug in the ws extension, and it provents me from sending message to my websocket server when events fired. will come back to this if it gets fixed when v2 of htmx comes out.

--- static/index.js
+++ static/index.js
@@ -1,32 +1,35 @@
-const wsStatus = document.querySelector("#wsStatus");
-const messageBox = document.querySelector("#messageBox");
-const message = document.querySelector("#message");
-const messages = document.querySelector("#messages");
-const currentUser = document.querySelector("#current_user");
-
-console.log(htmx);
-
-document.body.addEventListener("htmx:wsConnecting", function(_) {
-  wsStatus.innerHTML = "Connecting...";
-});
-
+// const wsStatus = document.querySelector("#wsStatus");
+// const messageBox = document.querySelector("#messageBox");
+// const message = document.querySelector("#message");
+// const messages = document.querySelector("#messages");
+// const currentUser = document.querySelector("#current_user");
+//
+// document.body.addEventListener("htmx:wsConnecting", function(_) {
+//   wsStatus.innerHTML = "Connecting...";
+// });
+//
 document.body.addEventListener("htmx:wsOpen", function(e) {
   e.preventDefault();
-  wsStatus.innerHTML = "Connected";
-  e.detail.socketWrapper.send("Work plz");
+  const msg = JSON.stringify({ HEADERS: { action: "Poop" } });
+  e.detail.socketWrapper.send(msg);
+  // wsStatus.innerHTML = "Connected";
 });
-
-document.body.addEventListener("htmx:wsAfterMessage", function(e) {
-  const sw = e.detail.socketWrapper;
-  messages.scrollIntoView({
-    block: "end",
-    behavior: "smooth",
-    inline: "nearest",
-  });
-
-  sw.send("Poop");
-});
-
-document.body.addEventListener("htmx:wsAfterSend", function(_) {
-  messageBox.value = "";
-});
+//
+// document.body.addEventListener("htmx:wsAfterMessage", function(e) {
+//   e.preventDefault();
+//   console.log(e.detail);
+//   messages.scrollIntoView({
+//     block: "end",
+//     behavior: "smooth",
+//     inline: "nearest",
+//   });
+// });
+//
+// document.body.addEventListener("htmx:wsAfterSend", function(_) {
+//   messageBox.value = "";
+// });
+//
+// document.body.addEventListener("htmx:wsClose", function() {
+//   htmx.trigger("#left");
+//   // e.detail.send("UO").then((res) => console.log(res));
+// });
